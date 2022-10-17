0518b39100feaf429ea2f5f49c3bef92944c9e51
Joshua V Arnaez <jarnaez@yondu.com>
2022-10-17 21:54:11 +0800

    ...change to signup

--- components/login/LoginFields.tsx
+++ components/login/LoginFields.tsx
@@ -147,7 +147,7 @@ const LoginFields = () => {
                     type="submit"
                     isLoading={isSubmitting}
                 >
-                    Login
+                    SignUp
                 </Button>
             </Flex>
         </>
